71474e46e99df0c7409d108fc80648af373f16ed
fatrianaa1 <63927438+fatrianaa1@users.noreply.github.com>
2020-04-18 14:32:34 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,10 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import plotly.graph_objs as go
+import pandas as pd
+import numpy as np
 
-########### Define your variables
+# Definir variables
 beers=['Chesapeake Stout', 'Snake Dog IPA', 'Imperial Porter', 'Double Dog IPA']
 ibu_values=[35, 60, 85, 75]
 abv_values=[5.4, 7.1, 9.2, 4.3]
