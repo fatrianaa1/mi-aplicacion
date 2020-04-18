e167e7d7becf25dccf021d42aa0b541e24f43f6a
fatrianaa1 <63927438+fatrianaa1@users.noreply.github.com>
2020-04-18 18:22:50 -0500

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ import plotly.graph_objs as go
 import pandas as pd
 import numpy as np
 
+
+
 # Definir tabla de datos:
 datos = pd.read_csv("https://gist.githubusercontent.com/fatrianaa1/76c832481a4fdb2e3683c2b232b5476f/raw/data_acciones.csv", delimiter = "\t")
 datos["Fecha"] = pd.to_datetime(datos["fecha"], format = "%d/%m/%Y %H:%M:%S")
@@ -13,11 +15,13 @@ datos = datos[datos["Cantidad"] > 0]
 lista_de_acciones = list(datos['Nemotecnico'].value_counts().sort_index().index)
 
 
+
 # Inicializar la aplicación:
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
 app.title= "Valkiria"
+
 
 
 # Configurar aplicación
@@ -34,52 +38,18 @@ app.layout = html.Div([
 
 
 
-
 # Definir callbacks:
-@app.callback(Output("graph_close")
-
-beers=['Chesapeake Stout', 'Snake Dog IPA', 'Imperial Porter', 'Double Dog IPA']
-ibu_values=[35, 60, 85, 75]
-abv_values=[5.4, 7.1, 9.2, 4.3]
-color1='lightblue'
-color2='darkgreen'
-mytitle='Beer Comparison'
-tabtitle='beer!'
-myheading='Flying Dog Beers'
-label1='IBU'
-label2='ABV'
-githublink='https://github.com/austinlasseter/flying-dog-beers'
-sourceurl='https://www.flyingdog.com/beers/'
-
-########### Set up the chart
-bitterness = go.Bar(
-    x=beers,
-    y=ibu_values,
-    name=label1,
-    marker={'color':color1}
-)
-alcohol = go.Bar(
-    x=beers,
-    y=abv_values,
-    name=label2,
-    marker={'color':color2}
-)
-
-beer_data = [bitterness, alcohol]
-beer_layout = go.Layout(
-    barmode='group',
-    title = mytitle
-)
-
-beer_fig = go.Figure(data=beer_data, layout=beer_layout)
-
-
-########### Initiate the app
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-server = app.server
-app.title=tabtitle
-
+@app.callback(Output('grafico_principal', 'figure'),
+              [Input('dropdown', 'value')])
+def grafica_principal(accion_seleccionada):
+    datos_seleccionados = datos[datos['Nemotecnico']==accion_seleccionada]
+    el_grafico_principal = go.Candlestick(x = datos_seleccionados["Fecha"], 
+                                          open = datos_seleccionados["Precio Medio"], 
+                                          high = datos_seleccionados["Precio Mayor"],
+                                          low = datos_seleccionados["Precio Menor"], 
+                                          close = datos_seleccionados["Precio Cierre"])
+    figura_principal = go.Figure(data = [el_grafico_principal])
+    return figura_principal
 
 if __name__ == '__main__':
     app.run_server()
