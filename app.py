93b983541c6c9dea3af3300e535dce54a6ebfeac
fatrianaa1 <63927438+fatrianaa1@users.noreply.github.com>
2020-04-19 15:52:54 -0500

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ lista_de_acciones = list(datos['Nemotecnico'].value_counts().sort_index().index)
 
 
 # Inicializar la aplicación:
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+external_stylesheets = ['https://cdn.rawgit.com/plotly/dash-app-stylesheets/2d266c578d2a6e8850ebce48fdb52759b2aef506/stylesheet-oil-and-gas.css']
 app = dash.Dash(__name__, external_stylesheets = external_stylesheets)
 server = app.server
 app.title= "Valkiria"
