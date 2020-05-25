119dd859dfc956ea357807224176a42f6f844343
fatrianaa1 <63927438+fatrianaa1@users.noreply.github.com>
2020-05-24 22:55:33 -0500

    Actualizada al 24 de mayo

--- app.py
+++ app.py
@@ -29,22 +29,87 @@ app.layout = html.Div([
         [
             html.Div(
                 [
+                    html.Img(
+                        src=app.get_asset_url("valkiria.png"),
+                        id="plotly-image",
+                        style={
+                            "height": "60px",
+                            "width": "auto",
+                            "margin-bottom": "25px",
+                        },
+                    )
+                ],
+                className="one-third column",
+            ),
+            html.Div(
+                [
                     html.Div(
                         [
-                            html.H2(
-                                'Análisis técnico de acciones de la BVC', style = {'margin-bottom': '0px'},
-                            ), 
+                            html.H3(
+                                "Análisis técnico de acciones de la BVC",
+                                style={"margin-bottom": "0px"},
+                            ),
                             html.H5(
-                                'Otra cosa', style = {'margin-top':'0px'}
+                                "Otra cosa", style={"margin-top": "0px"}
                             ),
                         ]
                     )
                 ],
-                className = 'one-half column',),
-        ], 
-        className = 'row flex-display'
+                className="one-half column",
+                id="title",
+            ),
+            html.Div(
+                [
+                    html.A(
+                        html.Button("Sobre el autor", id="learn-more-button"),
+                        href="https://www.linkedin.com/in/fabiantriana/",
+                    )
+                ],
+                className="one-third column",
+                id="button",
+            ),
+        ],
+        id="header",
+        className="row flex-display",
+        style={"margin-bottom": "25px"},
     ),
-    html.Div([html.H6(id="well_text"), html.P("No. of Wells")], id="wells", className="mini_container",),
+    html.Div(
+        [
+            html.Div(
+                [
+                    html.Div(
+                        [
+                            html.Div(
+                                [html.H6(id="well_text"), html.P("No. of Wells")],
+                                id="wells",
+                                className="mini_container",
+                            ),
+                            html.Div(
+                                [html.H6(id="gasText"), html.P("Gas")],
+                                id="gas",
+                                className="mini_container",
+                            ),
+                            html.Div(
+                                [html.H6(id="oilText"), html.P("Oil")],
+                                id="oil",
+                                className="mini_container",
+                            ),
+                            html.Div(
+                                [html.H6(id="waterText"), html.P("Water")],
+                                id="water",
+                                className="mini_container",
+                            ),
+                        ],
+                        id="info-container",
+                        className="row container-display",
+                    ),
+                ],
+                id="right-column",
+                className="eight columns",
+            ),
+        ],
+        className="row flex-display",
+    ),
     html.Div(
         [
             html.Div(
