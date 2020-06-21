c11f5700f2de12398561942e4141ec14bf1daa6c
fatrianaa1 <63927438+fatrianaa1@users.noreply.github.com>
2020-06-21 17:38:43 -0500

    Update app.py

--- app.py
+++ app.py
@@ -88,7 +88,7 @@ app.layout = html.Div([
                     html.Div(
                         [
                             html.Div(
-                                [html.H4(id="well_text"), 
+                                [html.H6(id="well_text"), 
                                  html.P("Variación")],
                                 id="wells",
                                 className="mini_container",
